c94ffa3d6f523a87fe1c551e190cc4c39649e557
Filipe Ferracini <filipe.ferracini@gmail.com>
2019-12-19 09:35:04 -0400

    First Commit

---
+++ Exercicio Resolvido/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Exercicio_Resolvido.Entities;
+
+namespace Exercicio_Resolvido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+            List<Employee> list = new List<Employee>();
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char outsourced = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double vph = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (outsourced == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double addCharge = double.Parse(Console.ReadLine());
+                    list.Add(new OutSourcedEmployee(name, hours, vph, addCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, vph));
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+        }
+    }
+}
